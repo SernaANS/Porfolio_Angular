eb3a4c4a9458592208dea8425d8cd191bf64bf5c
Santiago Serna <serna.ans051930@gmail.com>
2023-02-17 15:46:49 -0500

    new idea

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 
 import{ROUTES} from './app.routes';
 import { RouterModule} from '@angular/router';
-import { NavegationComponent } from './components/shared/navegation/navegation.component';
 import { HomeComponent } from './components/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule} from '@angular/material/toolbar';
@@ -14,24 +13,28 @@ import { MatSidenavModule} from '@angular/material/sidenav';
 import { MatListModule} from '@angular/material/list';
 import { MatIconModule} from '@angular/material/icon';
 import { MatButtonModule} from '@angular/material/button';
-import { PortafolioComponent } from './components/home/portafolio/portafolio.component';
-import { CardInfoComponent } from './components/home/card-info/card-info.component';
-import { ImagenComponent } from './components/home/card-info/imagen/imagen.component';
-import { InformationComponent } from './components/home/card-info/information/information.component';
-import { DevelopmentComponent } from './components/development/development.component';
-import { CertificateComponent } from './components/certificate/certificate.component';
+import { BanerComponent } from './components/home/baner/baner.component';
+import { PersonalInformationComponent } from './components/home/personal-information/personal-information.component';
+import { EducationComponent } from './components/home/education/education.component';
+import { WorkExperienceComponent } from './components/home/work-experience/work-experience.component';
+import { SkillsComponent } from './components/home/skills/skills.component';
+import { ContactsComponent } from './components/home/contacts/contacts.component';
+import { ProjectsComponent } from './components/home/projects/projects.component';
+import { ResponsiveDirective } from './components/home/directives/responsive.directive';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavegationComponent,
     HomeComponent,
-    PortafolioComponent,
-    CardInfoComponent,
-    ImagenComponent,
-    InformationComponent,
-    DevelopmentComponent,
-    CertificateComponent
+    BanerComponent,
+    PersonalInformationComponent,
+    EducationComponent,
+    WorkExperienceComponent,
+    SkillsComponent,
+    ContactsComponent,
+    ProjectsComponent,
+    ResponsiveDirective
   ],
   imports: [
     BrowserModule,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,10 @@
 import { Routes } from "@angular/router";
-import { CertificateComponent } from "./components/certificate/certificate.component";
-import { DevelopmentComponent } from "./components/development/development.component";
 import { HomeComponent } from "./components/home/home.component";
 
 export const ROUTES: Routes=[
     {path:'home',component:HomeComponent},
-    {path:'development',component:DevelopmentComponent},
-    {path:'certificate',component:CertificateComponent},
+    //{path:'development',component:DevelopmentComponent},
+    //{path:'certificate',component:CertificateComponent},
     // {path:'cartelera/**',component:SearchComponent},
     {path:'',pathMatch:'full',redirectTo:'home'},
     {path:'**',pathMatch:'full', redirectTo:'home'}
