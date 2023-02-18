8759a4863b2f03f4aeac0a478534af6e53b4a4d9
Santiago Serna <serna.ans051930@gmail.com>
2023-02-18 11:23:01 -0500

    personal information

--- src/app/components/home/personal-information/personal-information.component.ts
+++ src/app/components/home/personal-information/personal-information.component.ts
@@ -7,6 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonalInformationComponent implements OnInit {
 
+  myData: String[][]=[
+    ['Name', 'Santiago Serna'],
+    ['DOB','05/10/2000'],
+    ['Work Exp','2 years'],
+    ['Education','Software Enginner'],
+    ['Interests','Rugby']
+  ]
+
+  aboutMe: string[]=[
+    'Hi,  I am a Software Enginner with 2 years of experience  in software industry',
+    'Worked as developer of mobile in different technologies( Flutter, Java, Kotlin)',
+    'also worked as developer of fullstack in PHP(CodeIgniter) and Angular ',
+    'but I want to be developer of backend in JAVA whit SpringBoot or other technologies'
+
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
